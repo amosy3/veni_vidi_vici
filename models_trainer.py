bb30163db3c1ad73ce9d997b54ea11528e607651
amosy3 <amosy3@gmail.com>
2019-05-07 00:05:46 +0300

    add folders

--- models_trainer.py
+++ models_trainer.py
@@ -5,15 +5,19 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 # define all desirable models
-def create_models(df):
+def create_models(X):
     models_dict = dict()
 
-    for md in range(2,int(np.sqrt(df.shape[1])), 5):
+    for md in range(2,int(np.sqrt(X.shape[1])), 5):
         for criterion in ['gini', 'entropy']:
             models_dict['random_forest_'+str(md)+'_'+criterion] = \
                 RandomForestClassifier(n_estimators=500, max_depth=md, criterion=criterion)
 
-def train_models(X, y, models_dict):
+    return models_dict
+
+
+def train_models(X, y):
+    models_dict = create_models(X)
     for model_name, model in models_dict.items():
         print(model_name + ' is training...')
         model.fit(X,y)
